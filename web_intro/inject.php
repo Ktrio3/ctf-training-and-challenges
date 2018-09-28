a61116578d3fa0f9195ff6a163492ed6bcdb6731
Kevin Dennis <kevindennis@mail.usf.edu>
2018-09-28 15:07:50 -0700

    Added powerpoint and files

--- web_intro/inject.php
+++ web_intro/inject.php
@@ -3,9 +3,9 @@
 <?php
 // require("flag.txt") is easiest way, but you can do more complicated stuff :)
 // 1; $f = fopen("flag.txt", "r"); echo fread($f,filesize("flag.txt")); fclose($f)
-if( isset($_GET["equation"]) )
+if( isset($_POST["equation"]) )
 {
-  eval("\$a = " . $_GET["equation"] . ";");
+  eval("\$a = " . $_POST["equation"] . ";");
 }
 else
 {
@@ -15,7 +15,7 @@ else
 
 <html>
 <body>
-  <form action="inject.php">
+  <form action="inject.php" method="post">
     <p> Want to do some math? Enter an equation!</p>
     <textarea name="equation"></textarea><br/><br/>
     <input type="submit" value="Submit">
